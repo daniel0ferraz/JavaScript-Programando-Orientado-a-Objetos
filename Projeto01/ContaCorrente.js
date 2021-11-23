4a0d7549fe1aa23047ac4f6db2fba60e179a6606
Daniel Ferraz <Daniel0ferraz@outlook.com>
2021-11-23 11:09:02 -0300

    send to repository

---
+++ Projeto01/ContaCorrente.js
@@ -0,0 +1,44 @@
+
+export class ContaCorrente {
+  agencia;
+  cliente;
+
+  set cliente(novoValor) {
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor;
+    }
+  }
+
+  get cliente() {
+    return this.cliente;
+  }
+
+
+  // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+  _saldo = 0;
+
+  get saldo() {
+    return this.saldo;
+  }
+
+  sacar(valor) {
+    if (this._saldo >= valor) {
+      this._saldo -= valor;
+      return valor;
+    }
+  }
+
+  depositar(valor) {
+    if (valor <= 0) {
+      return;
+    }
+    this._saldo += valor;
+  }
+
+  tranferir(valor, conta) {
+
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
+
+  }
+}
